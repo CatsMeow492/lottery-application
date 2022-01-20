97b3d51fb791ce2951816083914f13687a93c5f0
CatsMeow492 <calisurf243@hotmail.com>
2022-01-20 14:38:53 -0800

    scaffolded

---
+++ contracts/Lottery.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.6.12;
+
+contract Lottery {
+    // Invest money by players - anyone in the world
+    // get balance - current balance
+    // manager clicks a function, it should
+        // select a player in random
+        // send the total money in the contract to the player
+}
